a35b0096add732f0c4c3587c24320175270cc2de
wyzdomcodes <aguowisdom@gmail.com>
2025-07-09 18:02:46 +0100

    feat: Implement movie search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,139 @@
-import { useEffect, useState } from 'react';
-import Header from './components/Header';
-import clsx from 'clsx';
+import { useEffect, useState } from "react";
+import Header from "./components/Header";
+import clsx from "clsx";
 
 function App() {
-  const [movies, setMovies] = useState([])
-  const [category, setCategory] = useState('popular')
-  const imgUrl = 'https://image.tmdb.org/t/p/w500'
-  const apiKey = import.meta.env.VITE_API_KEY
+  const [movies, setMovies] = useState([]);
+  const [category, setCategory] = useState("popular");
+  const imgUrl = "https://image.tmdb.org/t/p/w500";
+  const apiToken = import.meta.env.VITE_API_ACCESS_TOKEN;
+  const apiKey = import.meta.env.VITE_API_ACCESS_KEY;
+  const message = "We could not find the specific movie you were looking for!";
 
-  const getMovies = async (category) => {
-    await fetch(`https://api.themoviedb.org/3/movie/${category}?language=en-US&page=1`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${apiKey}`,
-      },
-    })
-    .then(response => response.json())
-    .then(data => {
-      setMovies(data.results)
-    })
-    .catch(error => console.log(error))
+  const [movie, setMovie] = useState("");
+
+  async function handleChange(e) {
+    e.preventDefault();
+    await fetch(
+      `https://api.themoviedb.org/3/search/movie?query=${movie}&api_key=${apiKey}`,
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.results.length > 0) {
+          setMovies(data.results);
+          setCategory("");
+          setMovie("");
+        } else {
+          setMovies([]);
+          setCategory("");
+          return message;
+        }
+      })
+      .catch((error) => console.log(error));
   }
 
+  const getMovies = async (category) => {
+    await fetch(
+      `https://api.themoviedb.org/3/movie/${category}?language=en-US&page=1`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${apiToken}`,
+        },
+      },
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setMovies(data.results);
+      })
+      .catch((error) => console.log(error));
+  };
+
   function handlePopular() {
-    setCategory('popular')
-      getMovies('popular')
+    setCategory("popular");
+    getMovies("popular");
   }
 
   function handleTopRated() {
-    setCategory('top_rated')
-      getMovies('top_rated')
+    setCategory("top_rated");
+    getMovies("top_rated");
   }
   function handleUpcoming() {
-    setCategory('upcoming')
-      getMovies('upcoming')
+    setCategory("upcoming");
+    getMovies("upcoming");
   }
   useEffect(() => {
-    handlePopular()
-  }, [])
+    handlePopular();
+  }, []);
 
   return (
     <>
-    <Header />
-    <main className='bg-[#121212] shadow-lg px-4 py-4'>
-      <div className="flex justify-start items-center gap-3 text-white pt-1 pb-2">
-        <button onClick={handlePopular} type='button' className={clsx('rounded-lg text-sm py-1.5 px-4 cursor-pointer', category === 'popular' ? "bg-[#EF4444]" : 'bg-[#1E1E1E]')}>Popular</button>
-        <button onClick={handleTopRated} type='button' className={clsx('rounded-lg text-sm py-1.5 px-4 cursor-pointer', category === 'top_rated' ? "bg-[#EF4444]" : 'bg-[#1E1E1E]')}>Top Rated</button>
-        <button onClick={handleUpcoming} type='button' className={clsx('rounded-lg text-sm py-1.5 px-4 cursor-pointer', category === 'upcoming' ? "bg-[#EF4444]" : 'bg-[#1E1E1E]')}>Upcoming</button>
-      </div>
-    <div className='grid sm:grid-cols-2 md:grid-cols-4 xl:grid-cols-5 gap-y-6 gap-x-5 place-content-center py-4'>
-      {movies.map(movie => {
-        return(
-          <div key={movie.id} className='rounded-xl shadow-movie-card max-sm:max-w-[450px]'>
-            <img src={imgUrl + movie.poster_path} alt={movie.title} className='rounded-md pb-3 '/>
-            <h3 className='font-inter text-white font-semibold text-sm'>{movie.title}</h3>
-            <p className='font-inter text-[#9CA3AF] font-light text-xs'>{movie.release_date}</p>
+      <Header movie={movie} setMovie={setMovie} handleChange={handleChange} />
+      <main className="bg-[#121212] shadow-lg px-4 py-4">
+        <div className="flex justify-start items-center gap-3 text-white pt-1 pb-2">
+          <button
+            onClick={handlePopular}
+            type="button"
+            className={clsx(
+              "rounded-lg text-sm py-1.5 px-4 cursor-pointer",
+              category === "popular" ? "bg-[#EF4444]" : "bg-[#1E1E1E]",
+            )}
+          >
+            Popular
+          </button>
+          <button
+            onClick={handleTopRated}
+            type="button"
+            className={clsx(
+              "rounded-lg text-sm py-1.5 px-4 cursor-pointer",
+              category === "top_rated" ? "bg-[#EF4444]" : "bg-[#1E1E1E]",
+            )}
+          >
+            Top Rated
+          </button>
+          <button
+            onClick={handleUpcoming}
+            type="button"
+            className={clsx(
+              "rounded-lg text-sm py-1.5 px-4 cursor-pointer",
+              category === "upcoming" ? "bg-[#EF4444]" : "bg-[#1E1E1E]",
+            )}
+          >
+            Upcoming
+          </button>
+        </div>
+        <div className="grid sm:grid-cols-2 md:grid-cols-4 xl:grid-cols-5 gap-y-6 gap-x-5 place-content-center py-4">
+          {movies.map((movie) => {
+            return (
+              <div
+                key={movie.id}
+                className="rounded-xl shadow-movie-card max-sm:max-w-[450px]"
+              >
+                <img
+                  src={imgUrl + movie.poster_path}
+                  alt={movie.title}
+                  className="rounded-md pb-3 "
+                />
+                <h3 className="font-inter text-white font-semibold text-sm">
+                  {movie.title}
+                </h3>
+                <p className="font-inter text-[#9CA3AF] font-light text-xs">
+                  {movie.release_date}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+
+        {movies.length === 0 && (
+          <div className="flex justify-center items-center h-95">
+            <p className="text-xl text-white font-bold text-center">
+              {message}
+            </p>
           </div>
-        )
-      })}
-    </div>
-    </main>
+        )}
+      </main>
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,28 @@
-import searchIcon from '../assets/search1.png'
-export default function Header () {
-    return (
-        <div className="py-8">
-            <form action="" className="h-12 bg-[#3b3b3b] mx-4 rounded-lg flex items-center justify-between gap-2 px-4">
-                <img src={searchIcon} alt="search icon" className="inline-block h-4 w-4"/>
-                <input type="text" placeholder="Search movies by title..." className="flex-1 placeholder:text-[#9CA3AF] placeholder:text-sm placeholder:font-light font-inter font-medium outline-none text-white"/>
-            </form>
-        </div>
-    )
+import searchIcon from "../assets/search1.png";
+
+export default function Header({ setMovie, handleChange, movie }) {
+  return (
+    <div className="py-8">
+      <form
+        onSubmit={handleChange}
+        className="h-12 bg-[#3b3b3b] mx-4 rounded-lg flex items-center justify-between gap-2 px-4"
+      >
+        <input
+          type="text"
+          value={movie}
+          name="movie"
+          onChange={(e) => setMovie(e.target.value)}
+          placeholder="Search movies by title..."
+          className="flex-1 placeholder:text-[#9CA3AF] placeholder:text-sm placeholder:font-light font-inter font-medium outline-none text-white"
+        />
+        <button type="button" className="cursor-pointer">
+          <img
+            src={searchIcon}
+            alt="search icon"
+            className="inline-block h-6 w-6"
+          />
+        </button>
+      </form>
+    </div>
+  );
 }
