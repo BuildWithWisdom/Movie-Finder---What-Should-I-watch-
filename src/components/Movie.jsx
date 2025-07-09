5896aff36e568d4bfb5a2dd2d28c33ed38f4e77f
wyzdomcodes <aguowisdom@gmail.com>
2025-07-10 00:56:54 +0100

    fix: added border radius to modal image

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -7,7 +7,7 @@ export default function Movie ({movie,setMovie, imgUrl, openModal, setOpenModal}
         <>
     {movie.id && <section className={clsx("flex justify-center items-center fixed top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] transition-all duration-1000 z-50", openModal ? "opacity-100 scale-100" : "opacity-0 scale-95")}>
              <div className="w-[768px] max-w-[768px] bg-[#1A1A1A] rounded-xl shadow-movie-card flex justify-between">
-                <img src={imgUrl + movie.poster_path} alt={movie.title} className="inline-block w-45 h-auto"/>
+                <img src={imgUrl + movie.poster_path} alt={movie.title} className="inline-block w-45 h-auto rounded-l-md"/>
                 <div className="p-8">
                     <h3 className="font-inter text-white font-bold text-2xl">{movie.title}</h3>
                     <p className="font-inter text-[#9CA3AF] font-light text-sm/[1.5rem] mt-4">{movie.overview}</p>
